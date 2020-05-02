05dd418af28b1012e5f768c2f0b72ca55373bfd2
raj-pat <rajp4445@gmail.com>
2020-05-01 17:33:15 -0700

    add config

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-const element = <h1>Hello World!</h1>;
-console.log(element);
-ReactDOM.render(element, document.getElementById("root"));
+// const element = <h1>Hello World!</h1>;
+// console.log(element);
+ReactDOM.render(<Counter />, document.getElementById("root"));
