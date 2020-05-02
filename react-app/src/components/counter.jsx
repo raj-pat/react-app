f3ffc5fbb55b5f83b8f739ee4af1a5845d1d12aa
raj-pat <rajp4445@gmail.com>
2020-05-01 19:28:56 -0700

    implement counter

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.increment = this.increment.bind(this);
+  }
+  state = {
+    count: 1,
+    url: "https://picsum.photos/200",
+  };
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <div>
+        <span style={{ fontSize: 10 }} className={this.getCountClasses()}>
+          {this.formatCount()}
+        </span>
+        <button onClick={this.increment} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+      </div>
+    );
+  }
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  getCountClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const count = this.state.count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
